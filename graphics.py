f21aafcd3824ae02fce2873c457d03c198d366e3
eeyako <eeyako42@gmail.com>
2024-08-19 21:18:45 -0700

    drawing some lines

--- graphics.py
+++ graphics.py
@@ -1,15 +1,39 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
+
+class Line:
+
+    def __init__(self, point0: Point, point1: Point) -> None:
+        self.point0 = point0
+        self.point1 = point1
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.point0.x,
+            self.point0.y,
+            self.point1.x,
+            self.point1.y,
+            fill=fill_color,
+            width=2
+        )
+
+
 class Window:
 
     def __init__(self, width: int, height: int) -> None:
         self.__root = Tk()
-        self.__root.title("Maze Solver")
+        self.__root.title(string="Maze Solver")
         self.__canvas = Canvas(height=height, width=width)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root.protocol(name="WM_DELETE_WINDOW", func=self.close)
 
     def redraw(self):
         self.__root.update_idletasks()
@@ -23,3 +47,6 @@ class Window:
 
     def close(self):
         self.running = False
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color=fill_color)
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
-    win = Window(800, 600)
+    win = Window(width=800, height=600)
+    line0 = Line(point0=Point(x=10, y=10), point1=Point(x=50, y=10))
+    line1 = Line(point0=Point(x=50, y=10), point1=Point(x=50, y=50))
+    line2 = Line(point0=Point(x=10, y=10), point1=Point(x=10, y=50))
+    line3 = Line(point0=Point(x=10, y=50), point1=Point(x=50, y=50))
+    win.draw_line(line=line0, fill_color="black")
+    win.draw_line(line=line1, fill_color="black")
+    win.draw_line(line=line2, fill_color="black")
+    win.draw_line(line=line3, fill_color="black")
     win.wait_for_close()
 
 
